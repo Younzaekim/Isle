f69a07bc6f51eece4ff05c909b4587564d95e28e
danseon <chlskrdnjs12@gmail.com>
2025-06-24 17:36:58 +0900

    fix : JumpLand 추가

--- Assets/Scripts/CNW_Scripts/SoundToAnimationTrigger.cs
+++ Assets/Scripts/CNW_Scripts/SoundToAnimationTrigger.cs
@@ -1,16 +1,25 @@
 using UnityEngine;
 
+[RequireComponent(typeof(PlayerController))]
 public class SoundToAnimationTrigger : MonoBehaviour
 {
     private SurfaceSensor surfaceSensor;
     private FootstepPlayer footstepPlayer;
+    private PlayerController playerController;
+
+    private bool wasGrounded = true;
+    private bool hasJumped = false;
 
     private void Awake()
     {
         surfaceSensor = GetComponentInChildren<SurfaceSensor>();
-        Debug.Log(surfaceSensor);
         footstepPlayer = GetComponentInChildren<FootstepPlayer>();
-        Debug.Log(footstepPlayer);
+        playerController = GetComponent<PlayerController>();
+    }
+
+    private void Update()
+    {
+        AnimationEvent_JumpLand();
     }
 
     public void AnimationEvent_Footstep()
@@ -20,11 +29,23 @@ public class SoundToAnimationTrigger : MonoBehaviour
 
     public void AnimationEvent_JumpStart()
     {
+        if (hasJumped) return;
+
         footstepPlayer.PlayJumpStart(surfaceSensor.CurrentSurface);
+        hasJumped = true;
     }
 
     public void AnimationEvent_JumpLand()
     {
-        footstepPlayer.PlayJumpLand(surfaceSensor.CurrentSurface);
+        bool isGrounded = playerController.isGrounded;
+
+        // 착지: 공중에서 지면으로 바뀌는 순간
+        if (isGrounded && !wasGrounded)
+        {
+            footstepPlayer.PlayJumpLand(surfaceSensor.CurrentSurface);
+            hasJumped = false;
+        }
+
+        wasGrounded = isGrounded;
     }
 }
