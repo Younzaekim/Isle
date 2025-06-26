3a0f8b9c89a9635fc9f6023026891046afb4b8dd
sonGit-Kraft <sdhunituser@gmail.com>
2025-06-26 18:22:47 +0900

    feat: add MainMenu UI

---
+++ Assets/Scripts/SDH_Scripts/UI_ButtonFloatTween.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+class UI_ButtonFloat : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    Vector3 originalPosition;
+    public float floatHeight = 10f;
+    public float floatSpeed = 5f;
+    bool isHovering = false;
+
+    void Start()
+    {
+        originalPosition = transform.localPosition;
+    }
+
+    void Update()
+    {
+        Vector3 target = isHovering ? originalPosition + Vector3.up * floatHeight : originalPosition;
+        transform.localPosition = Vector3.Lerp(transform.localPosition, target, Time.deltaTime * floatSpeed);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        isHovering = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isHovering = false;
+    }
+}
