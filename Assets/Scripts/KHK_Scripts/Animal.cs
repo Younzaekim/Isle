84737e21f06e6767c2450505ec27ca79dc6ea93a
Guy <khk3482@gmail.com>
2025-06-27 17:53:13 +0900

    개구리 애니메이션과 navmesh link 따라서 점프 및 상호작용

--- Assets/Scripts/KHK_Scripts/Animal.cs
+++ Assets/Scripts/KHK_Scripts/Animal.cs
@@ -5,11 +5,30 @@ public class Animal : MonoBehaviour
 {
     protected NavMeshAgent agent;
     protected Rigidbody rb;
+    protected Animator anim;
+    protected GameObject player;
+
+    protected bool isInteracted = false;
 
     protected void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    public virtual void Interact()
+    {
+        agent.isStopped = true;
+        agent.ResetPath();
 
-    }  
+        Vector3 lookDir = player.transform.position - transform.position;
+        lookDir.y = 0f;
+        if (lookDir.sqrMagnitude > 0.01f)
+        {
+            transform.rotation = Quaternion.LookRotation(lookDir);
+        }
+
+        isInteracted = true;
+    }
 }
