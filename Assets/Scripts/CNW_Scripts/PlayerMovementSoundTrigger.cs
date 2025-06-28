4ba73464d638f5800d33452f2d039788b196f229
danseon <chlskrdnjs12@gmail.com>
2025-06-28 23:52:20 +0900

    feat : PlayerSFX 완료

---
+++ Assets/Scripts/CNW_Scripts/PlayerMovementSoundTrigger.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerController_Test))]
+public class PlayerMovementSoundTrigger : MonoBehaviour
+{
+    private PlayerController_Test player;
+    private SurfaceSensor surfaceSensor;
+    private PlayerSFX playerSFX;
+
+    private bool wasGrounded = true;
+    private bool hasJumped = false;
+
+    private void Awake()
+    {
+        player = GetComponent<PlayerController_Test>();
+        surfaceSensor = GetComponentInChildren<SurfaceSensor>();
+        playerSFX = GetComponentInChildren<PlayerSFX>();
+    }
+
+    private void Update()
+    {
+        HandleJumpLandSound();
+        HandleFlyLoopSound();
+    }
+
+    public void AnimationEvent_Footstep()
+    {
+        playerSFX.PlayFootstep(surfaceSensor.CurrentSurface);
+    }
+
+    public void AnimationEvent_JumpStart()
+    {
+        if (hasJumped) return;
+
+        playerSFX.PlayJumpStart(surfaceSensor.CurrentSurface);
+        hasJumped = true;
+    }
+
+    private void HandleJumpLandSound()
+    {
+        bool isGrounded = player.isGrounded;
+
+        if (isGrounded && !wasGrounded)
+        {
+            playerSFX.PlayJumpLand(surfaceSensor.CurrentSurface);
+            hasJumped = false;
+        }
+
+        wasGrounded = isGrounded;
+    }
+
+    private void HandleFlyLoopSound()
+    {
+        if (player.isFlying)
+        {
+            playerSFX.PlayFlyLoop();
+        }
+        else
+        {
+            playerSFX.StopFlyLoop();
+        }
+
+        // 위치 갱신
+        playerSFX.UpdateFlyLoopPosition();
+    }
+}
